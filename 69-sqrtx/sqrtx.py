604b5c2604739eb83fdb11570267a0014059be5a
Raj Sharma <sharmaraj.sm@gmail.com>
2026-03-11 20:01:41 +0530

    Time: 1165 ms (10.42%) | Memory: 12.3 MB (97.54%) - LeetSync

---
+++ 69-sqrtx/sqrtx.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x<2:
+            return x
+        i = 2
+        while i*i<=x:
+            i+=1
+        return i-1
+        
