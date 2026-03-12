8f3768f87d7e1fc3e1b630e23e8b151fed2c8bb5
Raj Sharma <sharmaraj.sm@gmail.com>
2026-03-12 17:34:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Min Heap implementation/min-heap-implementation.java
@@ -0,0 +1,68 @@
+class minHeap {
+    
+    ArrayList<Integer> minHeap;
+    // Constructor
+    public minHeap() {
+        // Initialize your data members
+        minHeap = new ArrayList<>();
+    }
+    
+    public int getParent(int i){
+        return (i-1) / 2;
+    }
+    
+    public int getLeftChild(int i){
+        return 2*i + 1;
+    }
+    
+    public int getRightChild(int i){
+        return 2*i + 2;
+    }
+    
+    public void swap(int i, int j){
+        int temp = minHeap.get(i);
+        minHeap.set(i, minHeap.get(j));
+         minHeap.set(j, temp);
+    }
+
+    public void push(int x) {
+        // Insert x into the heap
+        minHeap.add(x);
+        int positionX = minHeap.size()-1;
+        while( positionX > 0 && getParent(positionX) >= 0 && minHeap.get(positionX) < minHeap.get(getParent(positionX)) ){
+            swap(positionX, getParent(positionX));
+            positionX = getParent(positionX);
+        }
+    }
+    
+    public void heapify(int i){
+        int smallest = i;
+        if(getLeftChild(i) < minHeap.size() && minHeap.get(getLeftChild(i))  < minHeap.get(smallest) ){
+            smallest = getLeftChild(i);
+        }
+        if( getRightChild(i) < minHeap.size() && minHeap.get(getRightChild(i)) < minHeap.get(smallest)){
+            smallest = getRightChild(i);
+        }
+        if(smallest != i){
+            swap(smallest, i);
+            heapify(smallest);
+        }
+    }
+    public void pop() {
+        // Remove the top (minimum) element
+        int lastIndex = minHeap.size()-1;
+        minHeap.set(0, minHeap.get(lastIndex));
+        minHeap.remove(lastIndex);
+        if(minHeap.size() > 0)
+            heapify(0);
+    }
+
+    public int peek() {
+        if(minHeap.size() > 0) return minHeap.get(0);
+        return -1;
+    }
+
+    public int size() {
+        return minHeap.size();
+    }
+}
