1011f09b54957081ec523143f20c972055924c02
Raj Sharma <sharmaraj.sm@gmail.com>
2026-04-08 22:07:32 +0530

    Time: 254 ms (69.09%) | Memory: 8.8 MB (70.37%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    bool solved = false;
+
+    bool safe(vector<vector<char>>& board, int row, int col, char num) {
+
+        for(int i = 0; i < 9; i++) {
+            if(board[row][i] == num) return false;
+        }
+
+        for(int i = 0; i < 9; i++) {
+            if(board[i][col] == num) return false;
+        }
+
+        int startRow = (row / 3) * 3;
+        int startCol = (col / 3) * 3;
+
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                if(board[startRow + i][startCol + j] == num) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    void solve(vector<vector<char>>& board) {
+        if(solved) return;
+    
+        for(int row = 0; row < 9; row++) {
+            for(int col = 0; col < 9; col++) {
+                if(board[row][col] == '.') {
+                    for(char num = '1'; num <= '9'; num++) {
+                        if(safe(board, row, col, num)) {
+                            board[row][col] = num;
+                            solve(board);
+                            if(solved) return;
+                            board[row][col] = '.';
+                        }
+                    }
+                    return;
+                }
+            }
+        }
+        solved = true;
+    }
+
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
+};
