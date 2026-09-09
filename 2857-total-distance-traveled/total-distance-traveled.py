4f92ca3bdaa4af3d6a7b72d4eb8703c79e059306
Raj Sharma <sharmaraj.sm@gmail.com>
2026-09-09 16:42:37 +0530

    Time: 9 ms (50.48%) | Memory: 19.3 MB (57.51%) - LeetSync

---
+++ 2857-total-distance-traveled/total-distance-traveled.py
@@ -0,0 +1,15 @@
+class Solution:
+    def distanceTraveled(self, mainTank: int, additionalTank: int) -> int:
+        distance = 0
+
+        while mainTank >= 5:
+            mainTank -= 5
+            distance += 50
+
+            if additionalTank > 0:
+                mainTank += 1
+                additionalTank -= 1
+
+        distance += mainTank * 10
+
+        return distance
